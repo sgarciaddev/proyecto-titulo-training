f861efd93a83c1582edd55011d33806eb2a5bfcd
Sebastián García D. <sebastian.garcia@pucv.cl>
2024-11-03 21:41:57 -0300

    [misc] - Documentación, requerimientos y librería interna. - Se genera librería interna para trabajar con los datos. - Se actualizan requerimientos de librerías externas en requirements.txt - Se actualiza documentación - Se actualizan archivos .env

--- ddosmllib/data/__init__.py
+++ ddosmllib/data/__init__.py
@@ -1,35 +1,44 @@
-from os import getenv
+from os import getenv, listdir
 from os.path import join as join_path, isfile
-from pandas import read_csv, DataFrame
+from enum import Enum
 from dotenv import load_dotenv
+from pandas import read_parquet
 
-class EnvVarNotDefinedError(Exception):
-    pass
-
+# Se cargan las variables de entorno (.env). En caso de que no existan, se lanza una excepción
 load_dotenv()
 if not getenv('DATA_SOURCE_PATH'):
-    raise EnvVarNotDefinedError('Environment variable DATASOURCE_PATH not defined')
+    raise EnvironmentError('La variable de entorno DATA_SOURCE_PATH no esta definida')
 if not getenv('TRAINED_MODELS_PATH'):
-    raise EnvVarNotDefinedError('Environment variable OUT_MODELS_PATH not defined')
+    raise EnvironmentError('La variable de entorno TRAINED_MODELS_PATH no esta definida')
+if not getenv('SAVED_PARQUET_PATH'):
+    raise EnvironmentError('La variable de entorno SAVED_PARQUET_PATH no esta definida')
 
-def get_dataset_fullpath(*args: str) -> str:
+class ArchivoProyecto(Enum):
     """
-    Permite obtener la ruta completa de un archivo de datos
-    :param args: Ruta del dataset a obtener
-    :return: Ruta completa del dataset
+    Enumeración de los archivos de datos del proyecto
     """
-    ds_path = getenv('DATA_SOURCE_PATH')
-    fullpath = join_path(ds_path, *args)
-    return fullpath
+    DATOS: str = 'DATA_SOURCE_PATH'
+    MODELOS_ENTRENADOS: str = 'TRAINED_MODELS_PATH'
+    ARCHIVOS_GUARDADOS: str = 'SAVED_PARQUET_PATH'
 
-def get_dataset_df(*args: str) -> DataFrame:
+
+class DatasetProyecto(Enum):
     """
-    Permite obtener un DataFrame de un archivo de datos
-    :param args: Ruta del dataset a obtener
-    :return: DataFrame del dataset, si existe, de lo contrario None
-    :raises FileNotFoundError: Si el archivo no existe
+    Enumeración de los datasets del proyecto
     """
-    ds_fullpath = get_dataset_fullpath(*args)
-    if not isfile(f'{ds_fullpath}.csv'):
-        raise FileNotFoundError(f'Dataset `{args[-1]}` from `{args[0]}` not found')
-    return read_csv(f'{get_dataset_fullpath(*args)}.csv', low_memory=False)
+    CIC_DDoS2019: str = 'cic-ddos2019'
+    N_BaIoT_RF: str = 'nbaiot-rf'
+    N_BaIoT_XGB: str = 'nbaiot-xgb'
+
+
+def get_dataset_df(dataset: DatasetProyecto):
+    """
+    Listar los archivos de un dataset
+    :param dataset: DatasetProyecto a listar
+    :return: DataFrame con los datos del dataset
+    """
+    dataset_path = join_path(getenv(ArchivoProyecto.ARCHIVOS_GUARDADOS.value),
+                             f'{dataset.value}.parquet')
+    if not isfile(dataset_path):
+        raise FileNotFoundError(f'El dataset {dataset.value} no esta registrado')
+    return read_parquet(dataset_path)
